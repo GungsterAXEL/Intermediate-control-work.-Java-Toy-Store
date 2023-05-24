dba1b486b96fcaaf8238ef6a0fe97762a5b2548c
Gun`gster <gungster.axel.ru@mail.ru>
2023-05-24 19:49:28 +0300

    little update

--- ToyStore/ToyFactory/Defaults.java
+++ ToyStore/ToyFactory/Defaults.java
@@ -2,7 +2,7 @@ package ToyStore.ToyFactory;
 
 public class Defaults {
     private static int titleCount = 15;
-    private static int maxUnitCount = 1;
+    private static int maxUnitCount = 100;
     private static int maxPrice = 10000;
     public static int getTitleCount() {
         return titleCount;
